f624f47387a90804e52502443eae4410dd4afda6
Christian Stussak <stussak@mfo.de>
2018-06-06 22:43:19 +0200

    Improve partition layout
    The BIOS boot partition now occupies less space and ends at the beginning of the ESP.

--- scripts/0010_create_partitions.sh
+++ scripts/0010_create_partitions.sh
@@ -6,9 +6,10 @@ truncate -s ${DISK_IMG_SIZE_MiB}m $DISK_IMG
 
 # Create the gpt partion table and partitions in $DISK_DEV.
 sgdisk \
-        -o \
+	-o \
+        -a 1024 \
+        -n 1::4095 -c 1:"BIOS Boot Partition" -t 1:ef02 \
         -a 4096 \
-        -n 1::+1M -c 1:"BIOS Boot Partition" -t 1:ef02 \
         -n 2::+128M -c 2:"EFI System Partition" -t 2:ef00 -A 2:set:2 \
         -n 3::+6G -c 3:"System Partition" -t 3:8300 \
         -n 4::+128M -c 4:"Configuration Partition" -t 4:0700 \
